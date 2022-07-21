af82b45ff35be41e39b818b67a7746b6134cb3e3
AOKA <amin.aoka@gmail.com>
2022-07-21 18:04:21 +0430

    practice

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-//DOM
-let time = document.getElementById("time");
+let inpKey = document.getElementById("inpKey");
+let inpValue = document.getElementById("inpValue");
+let insData = document.getElementById("insData");
+let lsOutput = document.getElementById ("lsOutput");
 
-//Functions
-let showtime = function (){
-let today= new Date();
-    hour= today.getHours()
-    min = today.getMinutes()
-    sec = today.getSeconds()
- time.innerHTML=`${hour} <span>:</span> ${min}<span>:</span> ${sec}`;
+insData.addEventListener("click",function(){
+    let key = inpKey.value;
+    let value = inpValue.value;
+    localStorage.setItem(key,value)
+    location.reload();
+   })
+
+   for(let i=0; i<localStorage.length; i++){
+    let key = localStorage.key(i)
+    let value = localStorage.getItem(key)
+    lsOutput.innerHTML += ` <li>${key} : ${value} </li>`
+
 }
-// use interva;
-setInterval(showtime,1000);
